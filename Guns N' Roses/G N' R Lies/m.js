b957a833e82298e65ead9f077033d3111603b171
littleflute <13699175164@sina.cn>
2017-07-09 13:36:26 -0500

    Add files via upload

---
+++ Guns N' Roses/G N' R Lies/m.js
@@ -0,0 +1,10 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "01 Reckless Life.mp3";		
+f[2]	= "02 Nice Boys.mp3";		
+f[3]	= "03 Move to the City.mp3";		
+f[4]	= "04 Mama Kin.mp3";		
+f[5]	= "06 Used to Love Her.mp3";		
+f[6]	= "07 You're Crazy.mp3";		
+f[7]	= "08 One in a Million.mp3";		
+f[8]	= "Patience.mp3";		
